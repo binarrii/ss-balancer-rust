05150d86f6c66b9717b34a3ef93a3ed4c38e70b6
dupp <dupp@wasu.com>
2021-01-13 15:49:39 +0800

    optimize, bug fix

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ fn select() -> Vec<&'static ProxyServer> {
     let i = COUNTER.fetch_add(1, Ordering::SeqCst);
     let s = serde_json::to_string(&selection).unwrap();
 
-    println!("{} SVR >> {}", i, s);
+    println!("{} >> {}", i, s);
 
     selection
 }
